872ea1a5a7859415b5f92635444c2558a626b022
zredhard <cleonartd@gmail.com>
2022-05-13 22:28:41 +0800

    [PATCH] Update

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,25 +1,3 @@
 <template>
-  <VueContainer>
-    {{ data_variable.name * 10 }}
-    <div class="w-40">
-      <a-input
-        @change="tes_1()"
-        v-model:value="data_variable.name"
-        placeholder="Basic usage"
-      />
-    </div>
-    <VueButton text="Create" type="indigo" @click="tes_1()" />
-  </VueContainer>
+  <router-view></router-view>
 </template>
-
-<script setup>
-import VueButton from "@/components/VueButton.vue";
-import VueContainer from "../components/VueContainer.vue";
-
-import { ref } from "vue";
-
-const data_variable = ref({
-  name: "",
-});
-function tes_1() {}
-</script>
