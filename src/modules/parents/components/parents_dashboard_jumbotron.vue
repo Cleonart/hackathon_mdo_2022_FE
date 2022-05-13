8d614c4efa4af66a5dccc290298786188373ff0c
citizenkidd <citizenkidd@gmail.com>
2022-05-14 02:54:02 +0800

    Update parents_dashboard_jumbotron.vue

--- src/modules/parents/components/parents_dashboard_jumbotron.vue
+++ src/modules/parents/components/parents_dashboard_jumbotron.vue
@@ -1,8 +1,18 @@
 <template>
     <!-- Bg white -->
-    <div class="max-w-screen-lg bg-white shadow-2xl rounded-lg mx-auto text-center py-12 mt-4">
-        <h2 class="text-3xl leading-9 font-bold tracking-tight text-gray-800 sm:text-4xl sm:leading-10">
-            Ayo daftarkan anakmu 
-        </h2>
+    <div class="max-w-screen-lg bg-white shadow-2xl rounded-lg mx-auto text-left py-12 mt-4 pl-10">
+        <h6 class="text-xl mb-1 font-semibold">
+        Keluarga Doe - Kruger
+        </h6>
+        <h6 class="text-xl mb-1 font-semibold">
+        NIK: 717100011
+        </h6>
+        <h4 class="text-xl mb-1 font-semibold">
+            Ayo Daftarkan Anakmu Untuk Imunisasi 
+        </h4>
+        <p class="mb-4 text-blueGray-500">
+        Papa dan mama jangan lupa cek jadwal imunisasi anak ya <br>
+        Imunisasi untuk melindungi anak di masa depan
+        </p>
     </div>
 </template>
