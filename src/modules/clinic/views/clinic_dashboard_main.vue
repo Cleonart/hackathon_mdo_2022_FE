6681ff1404dc8d4873ac42f06eda6b2969ba63be
Artquito <42540286+Artquito@users.noreply.github.com>
2022-05-14 00:43:35 +0800

    ABL finished table

--- src/modules/clinic/views/clinic_dashboard_main.vue
+++ src/modules/clinic/views/clinic_dashboard_main.vue
@@ -1,11 +1,46 @@
 <template>
   <clinic_dashboard_jumbotron />
   <clinic_dashboard_chart />
-  <clinic_dashboard_table />
+  <clinic_dashboard_table :dataSource="test_data" :columns="columns" />
 </template>
 
 <script setup>
 import clinic_dashboard_table from "../components/clinic_dashboard_table.vue";
 import clinic_dashboard_jumbotron from "../components/clinic_dashboard_jumbotron.vue";
 import clinic_dashboard_chart from "../components/clinic_dashboard_chart.vue";
+
+const test_data = [
+  {
+    test: "gagaga",
+  },
+  {
+    test: "gegege",
+  },
+];
+const columns = [
+  {
+    title: "Nama Anak",
+    dataIndex: "test",
+  },
+  {
+    title: "Umur",
+    dataIndex: "test",
+  },
+  {
+    title: "NIK",
+    dataIndex: "test",
+  },
+  {
+    title: "Tanggal Lahir",
+    dataIndex: "test",
+  },
+  {
+    title: "Tinggi",
+    dataIndex: "test",
+  },
+  {
+    title: "Berat",
+    dataIndex: "test",
+  },
+];
 </script>
