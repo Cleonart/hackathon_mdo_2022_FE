5f716b58f1b26ccf6caf8623caf901d4150d1399
Artquito <42540286+Artquito@users.noreply.github.com>
2022-05-14 01:53:23 +0800

    ABL unfinished chart

--- src/modules/clinic/components/clinic_dashboard_chart.vue
+++ src/modules/clinic/components/clinic_dashboard_chart.vue
@@ -1,1 +1,12 @@
-<template>Chart</template>
+<template>
+  <VueContainer>
+    <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+      <CardLineChart />
+    </div>
+  </VueContainer>
+</template>
+
+<script setup>
+import VueContainer from "@/components/VueContainer.vue";
+import CardLineChart from "../../../components/CardLineChart.vue";
+</script>
