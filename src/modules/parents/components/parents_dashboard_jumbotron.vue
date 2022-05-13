5a1bb1ce7f1cd3c8ac7d00405385e6a74f7d120a
citizenkidd <citizenkidd@gmail.com>
2022-05-14 03:27:55 +0800

    Update parents_dashboard_jumbotron.vue

--- src/modules/parents/components/parents_dashboard_jumbotron.vue
+++ src/modules/parents/components/parents_dashboard_jumbotron.vue
@@ -1,18 +1,16 @@
 <template>
     <!-- Bg white -->
-    <div class="max-w-screen-lg bg-white shadow-2xl rounded-lg mx-auto text-left py-12 mt-4 pl-10">
-        <h6 class="text-xl mb-1 font-semibold">
-        Keluarga Doe - Kruger
+    <div class=" bg-white shadow-2xl rounded-lg mx-auto text-left py-12 mt-4 pl-10">
+        <h6 class="text-xl font-normal leading-normal mt-0 mb-2 text-blueGray-800">
+            Keluarga Doe - Kruger <br>
+            NIK: 717100011
         </h6>
-        <h6 class="text-xl mb-1 font-semibold">
-        NIK: 717100011
-        </h6>
-        <h4 class="text-xl mb-1 font-semibold">
+        <h5 class="text-2xl font-normal leading-normal mt-0 mb-2 text-blueGray-800">
             Ayo Daftarkan Anakmu Untuk Imunisasi 
-        </h4>
-        <p class="mb-4 text-blueGray-500">
-        Papa dan mama jangan lupa cek jadwal imunisasi anak ya <br>
-        Imunisasi untuk melindungi anak di masa depan
+        </h5>
+        <p class="text-base font-light leading-relaxed text-emerald-800">
+            Papa dan mama jangan lupa cek jadwal imunisasi anak ya <br>
+            Imunisasi untuk melindungi anak di masa depan
         </p>
     </div>
 </template>
