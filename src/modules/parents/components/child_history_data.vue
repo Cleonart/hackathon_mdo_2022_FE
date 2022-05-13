bd0646170eba9a5a0d1f3869c23887cffdc5aa90
zredhard <cleonartd@gmail.com>
2022-05-14 02:13:38 +0800

    [PATCH] Child Data Update

---
+++ src/modules/parents/components/child_history_data.vue
@@ -0,0 +1,40 @@
+<!-- Personal Data -->
+<template>
+  <div class="md:col-span-1">
+    <div class="px-4 sm:px-0">
+      <h3 class="text-lg font-medium leading-6 text-gray-900">
+        Riwayat Imunisasi Anak
+      </h3>
+      <p class="mt-1 text-sm text-gray-600">Pantau kesehatan anak anda</p>
+    </div>
+  </div>
+  <div class="mt-5 md:mt-0 md:col-span-2">
+    <a-table :columns="table_columns" :dataSource="props.dataSource" bordered />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps(["data"]);
+const input_style =
+  "mt-1 pl-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md";
+
+const table_columns = [
+  {
+    title: "Tanggal Vaksinasi",
+    dataIndex: "test",
+  },
+  {
+    title: "Umur",
+    dataIndex: "test",
+  },
+  {
+    title: "Vaksin",
+    dataIndex: "test",
+  },
+  {
+    title: "Vaksinator",
+    dataIndex: "test",
+  },
+];
+</script>
---
+++ src/modules/parents/views/child_detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-8">
+    <p class="text-2xl font-bold mb-1">Detail Rekam Medis Anak</p>
+    <p class="mb-8">Silahkan mengecek riwayat rekam medis anak disini</p>
+    <hr />
+    <div class="mt-8">
+      <!-- Personal Data -->
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <ChildPersonal :data="{}" />
+        <ChildVaccinationData :data="[]" />
+        <ChildHistoryData :data="[]" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ChildPersonal from "../components/child_personal_data.vue";
+import ChildVaccinationData from "../components/child_vaccination_data.vue";
+import ChildHistoryData from "../components/child_history_data.vue";
+const input_style =
+  "mt-1 pl-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md";
+</script>
