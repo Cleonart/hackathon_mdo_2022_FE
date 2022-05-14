cc2e6d727106a12964e55e8fd5f6234f2bdd627c
Artquito <42540286+Artquito@users.noreply.github.com>
2022-05-14 10:37:03 +0800

    Update VueNavbar.vue

--- src/components/VueNavbar.vue
+++ src/components/VueNavbar.vue
@@ -3,12 +3,7 @@
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div
-          class="
-            flex-1 flex
-            items-center
-            justify-center
-            sm:items-stretch sm:justify-start
-          "
+          class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start"
         >
           <div class="flex-shrink-0 flex items-center">
             <h2 class="text-2xl text-white mb-0">MyVaccine</h2>
@@ -16,10 +11,11 @@
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
               <a
-                v-for="route in route_list"
+                v-for="(route, index) in route_list"
                 v-bind:key="route"
                 :href="route.route"
                 :class="get_class(route.selected)"
+                @click="change_selected(index)"
               >
                 {{ route.name }}
               </a>
@@ -32,7 +28,7 @@
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, reactive } from "vue";
 
 const props = defineProps(["page_title"]);
 const get_class = (selected) => {
@@ -43,7 +39,15 @@ const get_class = (selected) => {
   return class_name;
 };
 
-const route_list = [
+const change_selected = (selected_route) => {
+  route_list.forEach((route_obj) => {
+    route_obj.selected = false;
+  });
+  route_list[selected_route].selected = true;
+  console.log(route_list);
+};
+
+const route_list = reactive([
   {
     route: "/#/parents",
     selected: true,
@@ -54,5 +58,5 @@ const route_list = [
     selected: false,
     name: "Anak",
   },
-];
+]);
 </script>
