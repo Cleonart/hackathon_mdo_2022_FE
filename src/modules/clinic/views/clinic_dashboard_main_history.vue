e7c5976626fa9046607bfdf3929605584cf2ebf2
zredhard <cleonartd@gmail.com>
2022-05-14 09:42:39 +0800

    [PATCH] Update Seeder

--- src/modules/clinic/views/clinic_dashboard_main_history.vue
+++ src/modules/clinic/views/clinic_dashboard_main_history.vue
@@ -2,9 +2,21 @@
   <ClinicDashboardCards :data="CHILDS" />
   <h2 class="font-bold text-2xl mb-1 mt-10">Riwayat Data Vaksinasi</h2>
   <p>Silahkan melihat daftar anak penerima imunisasi aktif disini</p>
-  <a-table class="mt-5" :columns="table_columns" :dataSource="CHILDS" bordered>
+  <a-table class="mt-5" :columns="table_columns" :dataSource="HISTORY" bordered>
     <template #action>
       <VueButton custom_class="w-full me-0" text="Detail" type="primary" />
+    </template>
+    <template #child_id="{ record }">
+      {{ record.child_id[1] }}
+    </template>
+    <template #state="{ record }">
+      <span :class="REF_CLASS[record.state]">{{ REF_DATA[record.state] }}</span>
+    </template>
+    <template #child_height="{ record }">
+      {{ record.child_height }} Cm
+    </template>
+    <template #child_weight="{ record }">
+      {{ record.child_weight }} Kg
     </template>
   </a-table>
 </template>
@@ -13,7 +25,20 @@
 import ClinicDashboardCards from "../components/clinic_card_stats.vue";
 import { ref } from "vue";
 import { CHILDS } from "../../../seeder/list_of_childs.js";
+import { HISTORY } from "../../../seeder/list_of_history.js";
 import VueButton from "../../../components/VueButton.vue";
+
+const REF_DATA = ref({
+  right_on_time: "TEPAT WAKTU",
+  pass_due_date: "TERLAMBAT",
+});
+
+const REF_CLASS = ref({
+  right_on_time:
+    "text-xs  bg-green-500   px-2  py-1  font-bold rounded rounded-xl text-white",
+  pass_due_date:
+    "text-xs  bg-red-500   px-2  py-1  font-bold rounded rounded-xl text-white",
+});
 
 const table_columns = [
   {
@@ -29,7 +54,8 @@ const table_columns = [
   },
   {
     title: "Nama Anak",
-    dataIndex: "name",
+    dataIndex: "child_id",
+    slots: { customRender: "child_id" },
   },
   {
     title: "Tanggal Lahir",
@@ -42,10 +68,17 @@ const table_columns = [
   {
     title: "Tinggi",
     dataIndex: "child_height",
+    slots: { customRender: "child_height" },
   },
   {
-    title: "Berat",
+    title: "Berat Badan",
     dataIndex: "child_weight",
+    slots: { customRender: "child_weight" },
+  },
+  {
+    title: "Status",
+    dataIndex: "state",
+    slots: { customRender: "state" },
   },
 ];
 </script>
---
+++ src/seeder/list_of_history_2.js
@@ -0,0 +1,1 @@
+export let HISTORY_SEC_2 = [];
